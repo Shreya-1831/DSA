263f7fa7a3b4d1a3f0a57bd3c3d4a53e15c80613
Shreya-1831 <160025323+Shreya-1831@users.noreply.github.com>
2026-08-17 06:18:18 +0530

    Add deletion method for the beginning of a linked list
    Implement a method to delete the first node from a singly linked list and display the list before and after deletion.

---
+++ LinkedList/SinglyLinkedList/DeleteAtBegin.java
@@ -0,0 +1,49 @@
+/* 
+OUTPUT 
+Before Deletion: 1 -> 2 -> 3 -> 4 -> 5 -> null
+After Deletion: 2 -> 3 -> 4 -> 5 -> null
+*/
+
+class Node {
+    int data;
+    Node next;
+    Node (int val) {
+      this.data = val;
+      this.next = null;
+    }
+}
+public class DeleteAtBegin {
+  static Node deleteAtBegin (Node head) {
+    if (head == null){
+      return null;
+    }
+    Node temp = head;
+    head = head.next;
+    temp = null;
+    return head;
+  }
+
+  static void display(Node head) {
+    Node temp = head;
+    if (temp == null) {
+      System.out.println("null");
+      return;
+    } else {
+      System.out.print(temp.data + " -> ");
+    }
+    display(temp.next);
+  }
+  
+  public static void main(String[] args) {
+    Node p = new Node(1);
+    p.next = new Node(2);
+    p.next.next = new Node(3);
+    p.next.next.next = new Node(4);
+    p.next.next.next.next = new Node(5);
+    System.out.print("Before Deletion: ");
+    display(p);
+    p = deleteAtBegin(p);
+    System.out.print("After Deletion: ");
+    display(p);
+  }
+}
