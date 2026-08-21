30437537bb2f1c5f20ee793db3494e6571d129d4
Shreya-1831 <160025323+Shreya-1831@users.noreply.github.com>
2026-08-21 10:40:41 +0530

    Implement insertion at end for doubly linked list
    This class implements a doubly linked list with a method to insert a new node at the end and a method to display the list elements.

---
+++ LinkedList/DoublyLinkedList/InsertAtEnd.java
@@ -0,0 +1,65 @@
+/*
+OUTPUT
+List elements before insertion at end: 
+1 <-> 2 <-> 3 <-> 4 <-> 5 <-> null
+List elements after insertion at end: 
+1 <-> 2 <-> 3 <-> 4 <-> 5 <-> 6 <-> null
+*/
+
+import java.util.*;
+class Node {
+  int data;
+  Node prev;
+  Node next;
+  Node (int d){
+    data = d;
+    prev = null;
+    next = null;
+  }
+}
+
+public class InsertAtEnd{
+  static Node insertion(Node head, int val) {
+    Node newNode = new Node(val);
+    if (head == null) {
+      head = newNode;
+    } 
+    Node temp = head;
+    while (temp.next != null) {
+      temp = temp.next;
+    }
+    temp.next = newNode;
+    newNode.prev = temp;
+    return head;
+  }
+  static void display(Node head) {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.data + " <-> ");
+      temp = temp.next;
+    }
+    System.out.print("null");
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    Node first = new Node(1);
+    Node second = new Node(2);
+    Node third = new Node(3);
+    Node fourth = new Node(4);
+    Node fifth = new Node(5);
+    first.next = second;
+    second.prev = first;
+    second.next = third;
+    third.prev = second;
+    third.next = fourth;
+    fourth.prev = third;
+    fourth.next = fifth;
+    fifth.prev = fourth;
+    System.out.println("List elements before insertion at end: ");
+    display(first);
+    first = insertion(first, 6);
+    System.out.println("List elements after insertion at end: ");
+    display(first);
+  }
+}
